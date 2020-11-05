4babce1225e4f85bffd8a86fd74e21d5f6a97eb1
unknown <joshalmasin88@gmail.com>
2020-11-04 17:36:46 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <h1 class="text-center my-3">Todo List:</h1>
+          <Card />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './componets/Cards'
+
+export default {
+  name: 'app',
+  components: {
+    Card
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/componets/Cards.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    {{ nameText }}
+    <div class="text-center">
+      <input type="text" v-model="todo" class="form-control" placeholder="Add To Do..">
+      <button class="btn btn-primary mt-2 mb-2" @click="addTodo">Add Todo</button>
+    </div>
+    <div class="card" v-for="(item,index) in todoList" :key="index">
+      <p class="card-title text-right p-2 text-danger" @click="removeTodo(index)"><strong>X</strong></p>
+      <h4 class="card-title text-center mt-2">{{ index }}</h4>
+      <p class="card-text p-4 text-center">
+        {{ item }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['nameText '],
+  data() {
+    return {
+      todo: '',
+      todoList: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todoList.push(this.todo)
+      this.todo = ""
+    },
+    removeTodo(index) {
+      console.log(index)
+      this.todoList.splice(index,1)
+      this.todo = ""
+    }
+  }
+};
+</script>
+
